fd3ec6591b01f85718ee66f918d2765353b29711
dnisaev <dnisaev@yandex.ru>
2023-09-27 00:47:39 +0300

    update folders #5

---
+++ CodeWars/8kyu/SumArrays.js
@@ -0,0 +1,11 @@
+// Sum Numbers
+function sum (numbers) {
+
+    let amount = 0;
+    let i = 0;
+      
+      for (i; i < numbers.length; i++) {            
+        amount = amount + numbers[i];
+      }
+    return amount;
+  };
