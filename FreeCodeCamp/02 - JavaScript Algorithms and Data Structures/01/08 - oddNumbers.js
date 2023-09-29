ab7ac2162f105adde0501789706f169ee6caf129
dnisaev <dnisaev@yandex.ru>
2023-09-29 18:57:42 +0300

    update 08 - oddNumbers.js

--- FreeCodeCamp/02 - JavaScript Algorithms and Data Structures/01/08 - oddNumbers.js
+++ FreeCodeCamp/02 - JavaScript Algorithms and Data Structures/01/08 - oddNumbers.js
@@ -3,3 +3,11 @@ const myArray = [];
 for (let i = 1; i < 11; i += 2){
   myArray.push(i);
 }
+
+// 
+// const myArray = [];
+
+// Only change code below this line
+// for (let i = 9; i >= 1; i -= 2) {
+//     myArray.push(i);
+// }
