4ff8029517a38d40627cc006b7c7086493c4d4b6
dnisaev <dnisaev@yandex.ru>
2023-09-30 12:34:49 +0300

    update 07 - for.js

--- FreeCodeCamp/02 - JavaScript Algorithms and Data Structures/01/07 - for.js
+++ FreeCodeCamp/02 - JavaScript Algorithms and Data Structures/01/07 - for.js
@@ -4,9 +4,14 @@
 //   myArray.push(i);
 // }
 
+//---------------------------------------
+
 const myArr = [2, 3, 4, 5, 6];
 
 let total = 0;
 for (let i = 0; i < myArr.length; i++) {
   total = total + myArr[i];
 }
+
+//---------------------------------------
+
