788cdd83ff66d3ea2e0d012be87def04a4519bc2
dnisaev <dnisaev@yandex.ru>
2023-09-30 22:01:54 +0300

    add evenOrOdd.js

---
+++ CodeWars/8kyu/evenOrOdd.js
@@ -0,0 +1,11 @@
+// function evenOrOdd(number) {
+//     if (number % 2 === 0) {
+//         return "Even"
+//     } else {
+//         return "Odd"
+//     }
+// }
+
+function evenOrOdd(number) {
+    return number % 2 === 0 ? "Even" : "Odd"
+}
