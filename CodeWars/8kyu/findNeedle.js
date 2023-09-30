e190cd3f65216a65a89afaf628f07d72004208a1
dnisaev <dnisaev@yandex.ru>
2023-09-30 18:32:28 +0300

    update findNeedle.js

--- CodeWars/8kyu/findNeedle.js
+++ CodeWars/8kyu/findNeedle.js
@@ -5,3 +5,11 @@ function findNeedle(haystack) {
         }
     }
 }
+
+function findNeedle(haystack) {
+    return `found the needle at position ${haystack.indexOf('needle')}`;
+}
+
+let haystack = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'needle']
+
+console.log(findNeedle(haystack))
