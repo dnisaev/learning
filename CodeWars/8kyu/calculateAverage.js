847cc5c04e3aac3adfa5c73e19e353d4c4240ff8
dnisaev <dnisaev@yandex.ru>
2023-10-01 21:10:52 +0300

    update calculateAverage.js

--- CodeWars/8kyu/calculateAverage.js
+++ CodeWars/8kyu/calculateAverage.js
@@ -1,11 +1,15 @@
 function findAverage(array) {
-
-    let sum = 0;
-
-    for(let i = 0; i < array.length; i++){
-      sum += array[i]; 
+    // your code here
+    
+    if (array == 0){
+      return 0
     }
-
+    
+    let sum = 0;
+    for(let i = 0; i < array.length; i++){
+      
+        sum += array[i]; 
+  
+    }
     return sum / array.length;
-
   }
