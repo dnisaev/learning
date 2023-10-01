742720a91298d942e4fb08cedac43c73e73391ae
dnisaev <dnisaev@yandex.ru>
2023-10-01 23:52:28 +0300

    add findMaximumMinimumValuesList.js

---
+++ CodeWars/JavaScript/8kyu/12 - findMaximumMinimumValuesList.js
@@ -0,0 +1,11 @@
+// var min = function(list){
+//     return Math.min(...list)
+// }
+
+// var max = function(list){
+    
+//     return Math.max(...list)
+// }
+
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
