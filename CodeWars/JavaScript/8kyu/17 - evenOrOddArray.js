8d95ecc943eaf6be2cdaf675df003934adfbde94
dnisaev <dnisaev@yandex.ru>
2023-10-01 23:28:15 +0300

    add evenOrOddArray.js

---
+++ CodeWars/JavaScript/8kyu/17 - evenOrOddArray.js
@@ -0,0 +1,17 @@
+function oddOrEven(array) {
+  
+    if (array == 0){
+      return "even"
+    }
+    
+    let sum = 0;
+    
+    for(let i = 0; i < array.length; i++){
+    
+     sum += array[i] 
+      
+    }
+    
+    return sum % 2 === 0 ? 'even' : 'odd'
+   
+  }
