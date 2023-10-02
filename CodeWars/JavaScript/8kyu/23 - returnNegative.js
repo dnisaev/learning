b88245b37105d21ab231e9438350918a3f8f5a1a
dnisaev <dnisaev@yandex.ru>
2023-10-02 23:07:13 +0300

    add returnNegative.js

---
+++ CodeWars/JavaScript/8kyu/23 - returnNegative.js
@@ -0,0 +1,9 @@
+// function makeNegative(num) {
+//     return (num === 0) ? 0 
+//       : (num > 0) ? -num
+//       : num  
+//   }
+
+function makeNegative(num) {
+    return num > 0 ? -num : num  
+  }
