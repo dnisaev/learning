8ab748be2526cb5337bb998cc4756f3c9fe5cbf0
dnisaev <dnisaev@yandex.ru>
2023-10-07 11:57:39 +0300

    update my-app #3

--- samurai/00/my-app/src/App.tsx
+++ samurai/00/my-app/src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import image from './images/cat.png';
-import {Rating} from "./components/Rating";
-import {PageTitle} from "./components/PageTitle";
-
 
 function App() {
-    return (
-        <div className="App">
-            <img src={image} alt={"My name is Cat"}/>
-            <PageTitle title={"Hi there!"}/>
-            <PageTitle title={"Please vote for the cats App"}/>
-            <Rating value={1} />
-            <Rating value={2} />
-            <Rating value={3} />
-            <Rating value={4} />
-            <Rating value={5} />
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- samurai/00/my-app/src/components/PageTitle.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-type PageTitleProps = {
-    title: string
-}
-export function PageTitle(props: PageTitleProps) {
-    return ( <h3>{props.title}</h3> )
-}
