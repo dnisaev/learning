470dfa3c50242eebd168eb02976c2b74439ae237
dnisaev <dnisaev@yandex.ru>
2023-10-08 16:52:20 +0300

    update app01

--- samurai/00/app01/src/App.tsx
+++ samurai/00/app01/src/App.tsx
@@ -8,14 +8,16 @@ import {PageTitle} from "./components/PageTitle";
 function App() {
     return (
         <div className="App">
-            <img src={image} alt={"My name is Cat"}/>
-            <PageTitle title={"Hi there!"}/>
-            <PageTitle title={"Please vote for the cats App"}/>
-            <Rating value={1} />
-            <Rating value={2} />
-            <Rating value={3} />
-            <Rating value={4} />
-            <Rating value={5} />
+            <div>
+                <img src={image} alt={"My name is Cat"}/>
+                <PageTitle title={"Hi there!"}/>
+                <PageTitle title={"Please vote for the cats App"}/>
+                <Rating value={1}/>
+                <Rating value={2}/>
+                <Rating value={3}/>
+                <Rating value={4}/>
+                <Rating value={5}/>
+            </div>
         </div>
     );
 }
