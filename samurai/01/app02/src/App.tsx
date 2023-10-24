902eedbc2d4a65e93f1d8d81ca0713b8ef84ae33
dnisaev <dnisaev@yandex.ru>
2023-10-24 22:14:32 +0300

    update samurai/01/app02/

--- samurai/01/app02/src/App.tsx
+++ samurai/01/app02/src/App.tsx
@@ -5,10 +5,10 @@ import React from 'react';
 
 const Navigation = () => {
     return (
-        <Menu>
-            <MenuItem>Item 1</MenuItem>
-            <MenuItem>Item 2</MenuItem>
-            <MenuItem>Item 3</MenuItem>
+        <Menu role="menu" aria-label={"menu"}>
+            <MenuItem role={"menuitem"}><a>Item 1</a></MenuItem>
+            <MenuItem role={"menuitem"}><a>Item 2</a></MenuItem>
+            <MenuItem role={"menuitem"}><a>Item 3</a></MenuItem>
         </Menu>
     );
 };
@@ -24,18 +24,18 @@ function App() {
                 <Title>Accessibility</Title>
                 <Form>
                     <div>
-                        <Label htmlFor="1">Label for field 1</Label>
-                        <Field id="1" placeholder="Field 1"/>
+                        <Label htmlFor="1" aria-describedby="1">Label for field 1</Label>
+                        <Field id="1" placeholder="Field 1" aria-placeholder="Field 1"/>
                     </div>
 
                     <div>
-                        <Label htmlFor="2">Label for field 2</Label>
-                        <Field id="2" placeholder="Field 2"/>
+                        <Label htmlFor="2" aria-describedby="2">Label for field 2</Label>
+                        <Field id="2" placeholder="Field 2" aria-placeholder="Field 2"/>
                     </div>
 
                     <div>
-                        <Label htmlFor="3">Label for field 3</Label>
-                        <Field id="3" placeholder="Field 3"/>
+                        <Label htmlFor="3" aria-describedby="3">Label for field 3</Label>
+                        <Field id="3" placeholder="Field 3" aria-placeholder="Field 3"/>
                     </div>
                 </Form>
             </Block>
@@ -51,32 +51,39 @@ const Bouncing = keyframes`
     transform: translateY(20%);
 `
 
-const Title = styled.div`
+const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   margin: 60px 0;
   animation: ${Bouncing} 1s linear infinite alternate;
 `;
 
-const Header = styled.div`
+const Header = styled.header`
   height: 80px;
   background-color: #e91e63;
   display: flex;
   justify-content: space-around;
   align-items: center;
+  
+  button {
+    background-color: transparent;
+  }
 `;
 
-const Menu = styled.div`
+const Menu = styled.ul`
   display: flex;
   gap: 15px;
+  list-style: none;
 `;
 
-const MenuItem = styled.div`
-  color: white;
-  font-weight: bold;
+const MenuItem = styled.li`
+  a {
+    color: white;
+    font-weight: bold;
+  }
 `;
 
-const LoginBtn = styled.div`
+const LoginBtn = styled.button`
   color: white;
   font-weight: bold;
   padding: 10px 20px;
@@ -84,14 +91,14 @@ const LoginBtn = styled.div`
   border-radius: 5px;
 `;
 
-const Block = styled.div`
+const Block = styled.section`
   flex-grow: 1;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-const Form = styled.div`
+const Form = styled.form`
   width: 100%;
   max-width: 500px;
   display: flex;
