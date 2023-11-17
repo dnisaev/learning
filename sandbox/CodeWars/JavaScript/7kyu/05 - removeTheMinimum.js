4fe5ec5258b87d6b3a31bef263ed642f7615722e
dnisaev <dnisaev@yandex.ru>
2023-11-18 00:06:59 +0300

    done 05 - removeTheMinimum.js

--- sandbox/CodeWars/JavaScript/7kyu/05 - removeTheMinimum.js
+++ sandbox/CodeWars/JavaScript/7kyu/05 - removeTheMinimum.js
@@ -1,26 +1,18 @@
 function removeSmallest(numbers) {
-    let smallestNumIndex = 0;
+  
+  const newArray = [...numbers];
+  
+  let minNum = newArray[0];
+  let minNumIndex = 0;
+  
+  for (let i = 0; i < newArray.length; i++) {
     
-    if (numbers[0] === 2 && 
-        numbers[1] === 2 &&
-        numbers[2] === 1 &&
-        numbers[3] === 2 &&
-        numbers[4] === 1) {
-      return [2,2,2,1];
-    };
-
-    for (let i = 0; i < numbers.length - 1; i++) {
-      if (numbers[i + 1] < numbers[i]) {
-        smallestNumIndex = i + 1;
-      }
-    };
-
-    if (numbers[0] <= numbers[smallestNumIndex]) {
-        numbers.splice(0, 1);
-    } else {
-        numbers.splice(smallestNumIndex, 1);
+    if (newArray[i] < minNum) {
+      minNum = newArray[i];
+      minNumIndex = i;
     }
-    return numbers;
   }
-
-console.log(removeSmallest([2,2,1,2,1]))
+  newArray.splice(minNumIndex,1);
+  
+  return newArray;
+  }
