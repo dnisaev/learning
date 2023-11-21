7efa7c6ed9b9dda3aba21bcd8778eb147e1d31ec
dnisaev <dnisaev@yandex.ru>
2023-11-21 13:36:50 +0300

    refactor 2 - 28 - squareNsum.js

--- sandbox/CodeWars/JavaScript/8kyu/28 - squareNsum.js
+++ sandbox/CodeWars/JavaScript/8kyu/28 - squareNsum.js
@@ -2,11 +2,10 @@ let numbers = [0, 3, 4, 5];
 
 function squareSum(numbers){
     
-    //let newArr = [...numbers]
     let sum = 0;
 
     for (let i = 0; i < numbers.length; i++) {
-        numbers[i] === 1 ? sum += 1 : sum += numbers[i] * numbers[i];
+        sum += numbers[i] ** 2;
     }
 
     return sum;
