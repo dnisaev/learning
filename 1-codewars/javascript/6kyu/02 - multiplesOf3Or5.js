729c704179f34552591c0cbe74805fb9ec5e6e18
dnisaev <dnisaev@yandex.ru>
2024-01-03 17:31:30 +0300

    refactor 02 - multiplesOf3Or5.js

--- 1-codewars/javascript/6kyu/02 - multiplesOf3Or5.js
+++ 1-codewars/javascript/6kyu/02 - multiplesOf3Or5.js
@@ -1,24 +1,37 @@
-let number = 100;
+let number = 10;
 
 console.log(solution(number));
 
 function solution(number) {
-    if (number <= 3) {
-        return 0
-    }
 
-    let array = [];
-    let result = [];
+    let sum = 0;
 
     for (let i = 1; i < number; i++) {
-        array.push(i);
-    }
-
-    for (let j = 0; j <= array.length; j++) {
-        if (array[j] % 3 === 0 || array[j] % 5 === 0) {
-            result.push(array[j])
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
         }
     }
 
-    return result.reduce((acc, num) => acc + num);
+    return sum;
 }
+
+// function solution(number) {
+//     if (number <= 3) {
+//         return 0
+//     }
+//
+//     let array = [];
+//     let result = [];
+//
+//     for (let i = 1; i < number; i++) {
+//         array.push(i);
+//     }
+//
+//     for (let j = 0; j <= array.length; j++) {
+//         if (array[j] % 3 === 0 || array[j] % 5 === 0) {
+//             result.push(array[j])
+//         }
+//     }
+//
+//     return result.reduce((acc, num) => acc + num);
+// }
