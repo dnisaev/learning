f2f67ae438e249542cd8b937bb577f79aaa9e6e6
dnisaev <dnisaev@yandex.ru>
2024-01-31 14:13:23 +0300

    add solution 06 - sumOfDigitsDigitalRoot.js

---
+++ 1-codewars/javascript/6kyu/06 - sumOfDigitsDigitalRoot.js
@@ -0,0 +1,19 @@
+function digitalRoot(n) {
+
+    const sumOfNum = Array
+        .from(n.toString())
+        .map(num => +num)
+        .reduce((acc, num) => acc + num);
+
+    return sumOfNum.toString().length > 1 ? digitalRoot(sumOfNum) : sumOfNum;
+}
+
+console.log(`${digitalRoot(493193)} - should be - 2`)
+
+// Senior Solution
+//
+// function digitalRootSeniorSolution(n) {
+//     return (n - 1) % 9 + 1;
+// }
+//
+// console.log(`${digitalRootSeniorSolution(493193)} - should be - 2`)
