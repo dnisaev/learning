7236ae2416c932a09984014a2d1a04f0a93351de
dnisaev <dnisaev@yandex.ru>
2024-05-14 15:20:17 +0300

    add 18 - jadenCasingStrings.js

---
+++ 1-codewars/javascript/7kyu/18 - jadenCasingStrings.js
@@ -0,0 +1,10 @@
+String.prototype.toJadenCase = function () {
+    return this
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')
+}
+
+let string = "How can mirrors be real if our eyes aren't real";
+
+console.log(string.toJadenCase())
